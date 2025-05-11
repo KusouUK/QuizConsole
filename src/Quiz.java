c047c5e7b36d82969bc1fca3b94f3685e0a501a7
João Vitor <orion26br@gmail.com>
2025-05-11 19:38:32 -0300

    some changes
    changed questions, answers and correctanswer to be private. Also changed answers type to List<String>

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,12 +1,22 @@
-public class Quiz {
-    public String question;
-    public String[] answers;
-    public String correctAnswer;
+import java.util.List;
 
-    public Quiz(String question, String[] answers, String correctAnswer) {
+public class Quiz {
+    private String question;
+    private List<String> answers;
+    private String correctAnswer;
+
+    public Quiz(String question, List<String> answers, String correctAnswer) {
         this.question = question;
         this.answers = answers;
         this.correctAnswer = correctAnswer;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
     }
 
     public String getQuestion() {
@@ -15,14 +25,6 @@ public class Quiz {
 
     public void setQuestion(String question) {
         this.question = question;
-    }
-
-    public String[] getAnswers() {
-        return answers;
-    }
-
-    public void setAnswers(String[] answers) {
-        this.answers = answers;
     }
 
     public String getCorrectAnswer() {
