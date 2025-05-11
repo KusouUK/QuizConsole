a83f40a5f810c644d563ce2f6e499df2be08d7b8
João Vitor <orion26br@gmail.com>
2025-05-11 19:47:16 -0300

    rewrote to accomodate list

--- src/Questions.java
+++ src/Questions.java
@@ -1,13 +1,29 @@
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class Questions {
-    public static ArrayList<Quiz> getQuestions() {
-        ArrayList<Quiz> questions = new ArrayList<Quiz>();
-        questions.add(new Quiz("Who is Bjarne Stristrup", new String[]{"The creator of Python", "The creator of Java", "The creator of C#", "The creator of C++"}, "The creator of C++"));
-        questions.add(new Quiz("Quem é doc?", new String[]{"Gênio do python", "Vagabundo", "doido", "lindo"}, "doido"));
-        questions.add(new Quiz("True or false?", new String[]{"true", "false"}, "true"));
-        questions.add(new Quiz("Which number is the greatest?", new String[]{"1", "2", "3", "4", "5"}, "5"));
-
-        return questions;
+    public static List<Quiz> getQuestions() {
+        return List.of(
+                new Quiz(
+                        "Who is Bjarne Stristrup",
+                        Arrays.asList("The creator of Python", "The creator of Java", "The creator of C#", "The creator of C++"),
+                        "The creator of C++"
+                ),
+                new Quiz(
+                        "Quem é doc?",
+                        Arrays.asList("Gênio do python", "Vagabundo", "doido", "lindo"),
+                        "doido"
+                ),
+                new Quiz(
+                        "True or false?",
+                        Arrays.asList("true", "false"),
+                        "true"
+                ),
+                new Quiz(
+                        "Which number is the greatest?",
+                        Arrays.asList("1", "2", "3", "4", "5"),
+                        "5"
+                )
+        );
     }
 }
