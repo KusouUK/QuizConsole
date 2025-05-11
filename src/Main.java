85e568e500d3b7b0b52116721e5b2cd4ce2babaa
João Vitor <orion26br@gmail.com>
2025-05-11 19:48:28 -0300

    size() and get()
    Changed from length to size(), and used get() for it to work with list

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Quiz> questions = Questions.getQuestions();
+        List<Quiz> questions = Questions.getQuestions();
         Quiz randomQuestion = questions.get(new Random().nextInt(questions.size()));
 
         System.out.println(randomQuestion.question + "\n");
 
         String[] options = {"A", "B", "C", "D", "E"};
-        for(int i = 0; i < randomQuestion.answers.length; i++) {
-            System.out.println(options[i] + ". " + randomQuestion.answers[i]);
+        for(int i = 0; i < randomQuestion.answers.size(); i++) {
+            System.out.println(options[i] + ". " + randomQuestion.answers.get(i));
         }
 
         System.out.print("\nAnswer: ");
@@ -19,15 +19,15 @@ public class Main {
 
         boolean isValidAnswer = answer.chars()
                 .allMatch(c -> "abcde".indexOf(c) != -1);
-        if(answer.length() != 1 || !isValidAnswer || Arrays.asList(options).indexOf(answer.toUpperCase()) > randomQuestion.answers.length - 1) {
+        if(answer.length() != 1 || !isValidAnswer || Arrays.asList(options).indexOf(answer.toUpperCase()) > randomQuestion.answers.size() - 1) {
             System.out.println("Invalid answer");
             System.exit(0);
         }
 
-        answer = randomQuestion.answers[Arrays.asList(options).indexOf(answer.toUpperCase())];
+        answer = randomQuestion.answers.get(Arrays.asList(options).indexOf(answer.toUpperCase()));
 
         if(!Objects.equals(answer, randomQuestion.correctAnswer)) {
-            System.out.println("Wrong! The correct answer was " + options[Arrays.asList(randomQuestion.answers).indexOf(randomQuestion.correctAnswer)] + ". " + randomQuestion.correctAnswer);
+            System.out.println("Wrong! The correct answer was " + options[randomQuestion.answers.indexOf(randomQuestion.correctAnswer)] + ". " + randomQuestion.correctAnswer);
             System.exit(0);
         }
         System.out.println("You are correct!");
